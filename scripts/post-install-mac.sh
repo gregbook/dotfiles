520fe0b9b1b637a8cb45f2a8d335ecacf08eeee8
Gregor Arzouyan <gregor.arzouyan@pipedrive.com>
2021-11-10 21:15:45 +0200

    fixes for big sur

--- scripts/post-install-mac.sh
+++ scripts/post-install-mac.sh
@@ -1,7 +1,3 @@
-#### ******************************** CONFIGURE ZSH BREW INSTALL BY DEFAULT ****************
-sudo sh -c "echo $(which zsh) >> /etc/shells"
-chsh -s $(which zsh)
-
 #### ************************* MAC PARAMETERS CONFIGURATION ********************************
 echo "Setting up Mac configuration parameters"
 
@@ -9,17 +5,14 @@ echo "Setting up Mac configuration parameters"
 defaults write com.apple.screencapture type JPG
 
 ### FINDER
-# Show ~/Library in $HOME folder
-chflags nohidden ~/Library/
-
 # Sound effects
 defaults write NSGlobalDomain com.apple.sound.beep.sound /System/Library/Sounds/Blow.aiff
 
 # Show Path/Status Bar, List view, extensions
-defaults write com.apple.finder ShowStatusBar -bool true
-defaults write com.apple.finder FXPreferredViewStyle -string “Nlsv”
-defaults write com.apple.finder ShowPathbar -bool true
-sudo defaults write NSGlobalDomain AppleShowAllExtensions -bool true
+defaults write com.apple.finder ShowStatusBar -bool "true"
+defaults write com.apple.finder FXPreferredViewStyle -string "Nlsv"
+defaults write com.apple.finder ShowPathbar -bool "true"
+defaults write NSGlobalDomain AppleShowAllExtensions -bool "true"
 
 # Find default folder to $HOME
 defaults write com.apple.finder NewWindowTarget -string "PfHm"
@@ -28,47 +21,53 @@ defaults write com.apple.finder NewWindowTargetPath -string "file://${HOME}/"
 # Search in current folder by default
 defaults write com.apple.finder FXDefaultSearchScope -string "SCcf"
 
+# Dark Theme
+defaults write "Apple Global Domain" "AppleInterfaceStyle" "Dark"
+
 # Quickview text selection
-defaults write com.apple.finder QLEnableTextSelection -bool true
+defaults write com.apple.finder QLEnableTextSelection -bool "true"
 
 # No .DS_STORE on drives
-defaults write com.apple.desktopservices DSDontWriteNetworkStores -bool true
-defaults write com.apple.desktopservices DSDontWriteUSBStores -bool true
+defaults write com.apple.desktopservices DSDontWriteNetworkStores -bool "true"
+defaults write com.apple.desktopservices DSDontWriteUSBStores -bool "true"
 
 # Ask Password right after save screen
 defaults write com.apple.screensaver askForPassword -int 1
 defaults write com.apple.screensaver askForPasswordDelay -int 0
 
 # Keyboard repetition delay
-sudo defaults write NSGlobalDomain KeyRepeat -int 1
+defaults write NSGlobalDomain KeyRepeat -int 1
 # Keystroke time delay
-sudo defaults write NSGlobalDomain InitialKeyRepeat -int 10
+defaults write NSGlobalDomain InitialKeyRepeat -int 10
 
 ## CONTROL CENTER SETTINGS
-defaults write ~/Library/Preferences/ByHost/com.apple.controlcenter.plist BatteryShowPercentage -bool true
+defaults write ~/Library/Preferences/ByHost/com.apple.controlcenter.plist BatteryShowPercentage -bool "true"
 defaults write ~/Library/Preferences/ByHost/com.apple.controlcenter.plist Sound -int 18
-defaults write ~/Library/Preferences/ByHost/com.apple.controlcenter.plist NowPlaying -int 2
+defaults write ~/Library/Preferences/ByHost/com.apple.controlcenter.plist NowPlaying -int 18
 
 ## TOUCHBAR SETTINGS
 defaults write com.apple.touchbar.agent PresentationModeGlobal -string fullControlStrip
 defaults write com.apple.touchbar.agent PresentationModeFnModes '<dict><key>fullControlStrip</key><string>functionKeys</string></dict>'
 defaults write com.apple.controlstrip FullCustomized '("com.apple.system.group.brightness","com.apple.system.group.keyboard-brightness","com.apple.system.group.media","com.apple.system.group.volume","com.apple.system.screen-lock","com.apple.system.do-not-disturb")'
-sudo pkill ControlStrip TouchBarServer
 
 ## DOCK Settings
+# Auto hide
+defaults write com.apple.dock "autohide" -bool "true"
 # No delay
-defaults write com.apple.dock.autohide-delay -float 0
+defaults write com.apple.dock "autohide-delay" -float "0"
 # Minimum text size
 defaults write com.apple.dock tilesize -int 45
 # Magnification off
-defaults write com.apple.dock magnification -bool false
+defaults write com.apple.dock magnification -bool "false"
 
 ## HOT CORNERS
+defaults write com.apple.dock "mru-spaces" -bool "false"
+
 # TOP LEFT : Mission Control
 defaults write com.apple.dock wvous-tl-corner -int 2
 defaults write com.apple.dock wvous-tl-modifier -int 0
 # TOP RIGHT : - 
-defaults write com.apple.dock wvous-tr-corner -int 1
+defaults write com.apple.dock wvous-tr-corner -int 2
 defaults write com.apple.dock wvous-tr-modifier -int 0
 # DOWN LEFT : Desktop
 defaults write com.apple.dock wvous-bl-corner -int 4
@@ -76,6 +75,8 @@ defaults write com.apple.dock wvous-bl-modifier -int 0
 # DOWN RIGHT : Lock
 defaults write com.apple.dock wvous-br-corner -int 13
 defaults write com.apple.dock wvous-br-modifier -int 0
+
+defaults write com.apple.finder ShowHarDrivesOnDesktop -bool "true"
 
 ## ADD SOME APPS TO DOCK
 defaults write com.apple.dock persistent-apps -array
@@ -91,7 +92,6 @@ defaults write com.apple.dock persistent-apps -array-add '<dict><key>tile-data</
 
 ## Restart Dock/Finder & Reboot
 echo "Finder et Dock restarted... please reboot!"
-killall Dock
-killall Finder
+pkill Docker Finder ControlStrip TouchBarServer
 
 echo 0 > ./scripts/executed.txt
